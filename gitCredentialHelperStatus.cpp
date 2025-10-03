f89b5a51286d42109ced1b1522f0c41377414a5b
shn <shn@192.168.1.13>
2025-10-04 02:59:28 +0530

    Git Credential Helper Working

--- gitCredentialHelperStatus.cpp
+++
@@ -1,8 +0,0 @@
-# include <iostream>
-int main()
-{
-
-std::cout<<" Testing git credential helper - store "<<std::endl;
-
-	return 0;
-}
---
+++ successGitCredentialHelper.cpp
@@ -0,0 +1,8 @@
+# include <iostream>
+int main()
+{
+
+std::cout<<" -- Worked as expected --"<<std::endl;
+
+	return 0;
+}
